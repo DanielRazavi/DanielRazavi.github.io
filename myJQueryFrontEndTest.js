4d542d108182072e5373cd898629a29547d07669
Daniel <daniel.razaviazarkhiavi@mail.utoronto.ca>
2023-04-29 18:07:40 -0400

    Finalized the index page.

--- myJQueryFrontEndTest.js
+++ myJQueryFrontEndTest.js
@@ -1,22 +1,17 @@
 $(function() {
     $('head').append('<link rel="stylesheet" href="newstyle.css" type="text/css" />');
 
-    $("#mainbox").addClass("main box").css({
-            'color': '#999999',
+    $("#mainbox").addClass("box").css({
+            'margin': '15% auto 15%',
+	        'text-align': 'center',
+            'color': 'transparent',
             'background-color': 'rgba(255,255,255,0.5)',
-            'box-shadow': '0 5px 35px 0px rgba(0,0,0,0.5)'
+            'box-shadow': '0 5px 35px 0px rgba(255, 255, 255, 0.5)'
         });
-    
-    $(".hint").css({
-        'color': '#999999',
-        'position':'absolute',
-        'font-size':'4vw'
-    }).hide();
-   
+    $(".hint").addClass("hint");
     function timeout(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
-    //  For .hint's animation
     setInterval(async function() {
         $(".hint").animate({opacity: "0.0"}, 700, "linear");
         await timeout(700);
@@ -28,46 +23,66 @@ $(function() {
         $(".hint").html("Blank.");
         $(".hint").animate({opacity: "1.0"}, 1000, "linear");
     }, 10000);
-
-    $("#navbar").addClass("box navbar");
-   
-    $("button").css({
-        'width': '50%',
-        'padding': '12px 20px',
-        'margin': '15px 0',
-        'border': '1px solid #999999',
-        'border-radius': '8px',
-        'background-color':  'transparent',
-        'color':  '#999999',
-        'cursor': 'pointer',
-    });
-
     $("#mainbox").hover(function(){
         $(".hint").fadeOut(300, "linear");
         $("#mainbox").css({
             "box-shadow": "0 5px 35px 0px rgba(0,0,0,0.5)",
+            "color": "#999999",
             "transition": "1s 0.4s linear"
         })
-        $("#mainbox > :not(.hint)").delay(400).animate({
-            opacity: "1",
-        }, 1000, "linear");
+        $(".button").css({
+            'opacity': '1',
+            'color': '#999999',
+	        'border': '1px solid #999999',
+	        'transition': '1s 0.5s linear'
+        });
     }, function(){
         $("#mainbox").css({
             "box-shadow": "0 5px 35px 0px rgba(255, 255, 255, 0.5)",
+            "color": "transparent",
             "transition": "1s 0.5s linear"
         });
-        $("#mainbox > :not(.hint)").delay(500).animate({
-            opacity: "0.0"
-        }, 1000, "linear");
+        $(".button").css({
+            'opacity': '0',
+            'color': 'transparent',
+	        'border': '1px solid transparent',
+	        'transition': '1s 0.5s linear'
+        });
         $(".hint").delay(1600).fadeIn(300, "linear");
     });
 
+    $("button").addClass("button");
+    $("button").hover(function(){
+        $(this).css({
+            'color': '#ffffff',
+            'background-color': '#999999',
+            'border': '1px solid #999999',
+            'transition': '0.5s linear'
+        });
+    }, function(){
+        $(this).css({
+            'color': '#999999',
+            'background-color': 'transparent',
+            'border': '1px solid  #999999',
+            'transition': '1s linear'
+        });
+    });
+    $("button").on("click", function(){
+        window.location.href='mailto:danraz1376@gmail.com';
+    });
+
+    $("#navbar").addClass("box navbar");
+    $("#navbar > a").css({
+        'color': 'transparent',
+        'display': 'table-cell',
+        'text-align': 'center',
+    });
     $("#navbar").hover(function(){
         $(this).css({
             'width': '400px',
             'background-color': '#ffffff',
 		    'box-shadow': '0 5px 35px 0px rgba(0,0,0,0.5)',
-            "transition": "1s 0.4s"
+            "transition": "1s 0s"
         });
         $("#navbar > a").css({
             'color': '#999999',
@@ -88,7 +103,6 @@ $(function() {
             'background-color': 'transparent',
 		    'box-shadow': '0 5px 35px 0px rgba(255, 255, 255, 0.5)',
             "transition": "1s 0.6s"
-        });
-        
+        }); 
     });
 })
