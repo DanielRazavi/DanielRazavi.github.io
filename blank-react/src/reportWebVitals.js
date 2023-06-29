168be40d513685055bf867f7714e1f22683c821b
Daniel <daniel.razaviazarkhiavi@mail.utoronto.ca>
2023-06-29 18:17:04 -0400

    Making the barebone react project

---
+++ blank-react/src/reportWebVitals.js
@@ -0,0 +1,15 @@
+// I have commented this out inside of the index.js. For the time being will keep it but will not use it.
+
+const reportWebVitals = (onPerfEntry) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
