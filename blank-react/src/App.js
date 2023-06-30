304b7eec358061816db9131c8d0c28254c210525
Daniel <daniel.razaviazarkhiavi@mail.utoronto.ca>
2023-06-30 15:18:15 -0400

    Updated syntax from react-router-dom@5 to react-router-dom@6.

--- blank-react/src/App.js
+++ blank-react/src/App.js
@@ -1,30 +1,18 @@
 import Navbar from "./Navbar";
 import HomePage from "./HomePage";
 import PhotosPage from "./PhotosPage";
-import TutorialsPage from "./TutorialsPage";
-import ProjectsPage from "./ProjectsPage";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="app">
         <div className="content">
-          <Switch>
-            <Route exact path="/">
-              <HomePage />
-            </Route>
-            <Route path="/photos">
-              <PhotosPage />
-            </Route>
-            <Route path="/tutorials">
-              <TutorialsPage />
-            </Route>
-            <Route path="/projects">
-              <ProjectsPage />
-            </Route>
-          </Switch>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/photos" element={<PhotosPage />} />
+          </Routes>
         </div>
         <Navbar />
       </div>
