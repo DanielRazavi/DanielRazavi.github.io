8db0ed9dc038590077f2cd0de8e43218039f8ddc
Daniel <daniel.razaviazarkhiavi@mail.utoronto.ca>
2023-06-30 14:21:40 -0400

    Implementing the Application Component.

--- blank-react/src/App.js
+++ blank-react/src/App.js
@@ -1,9 +1,40 @@
-import logo from "./logo.svg";
+import Navbar from "./Navbar";
+import HomePage from "./HomePage";
+import PhotosPage from "./PhotosPage";
+import TutorialsPage from "./TutorialsPage";
+import ProjectsPage from "./ProjectsPage";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="app">
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/photos">
+              <PhotosPage />
+            </Route>
+            <Route path="/tutorials">
+              <TutorialsPage />
+            </Route>
+            <Route path="/projects">
+              <ProjectsPage />
+            </Route>
+          </Switch>
+        </div>
+        <Navbar />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
+/* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,8 +48,4 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
-
-export default App;
+    </div> */
