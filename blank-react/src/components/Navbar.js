85c5b6263c50e13601c2f3fb2fb4cb049e4d79b0
Daniel <daniel.razaviazarkhiavi@mail.utoronto.ca>
2023-07-04 13:55:25 -0400

    Created Navbar.module.css and incorporated it in Navbar.js

--- blank-react/src/components/Navbar.js
+++ blank-react/src/components/Navbar.js
@@ -1,8 +1,25 @@
+import style from "./Navbar.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Navbar() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleHover = () => {
+    setIsHovered(true);
+  };
+
+  const handleLeave = () => {
+    setIsHovered(false);
+  };
+
+  const navbarClassName = `${style.navbar} ${isHovered ? style.hovered : ""}`;
+
   return (
-    <div className="navbar">
+    <div
+      className={navbarClassName}
+      onMouseEnter={handleHover}
+      onMouseLeave={handleLeave}>
       <Link to="./">Home</Link>
       <Link to="./photos">Photos</Link>
       <Link to="./tutorials">Tutorials</Link>
